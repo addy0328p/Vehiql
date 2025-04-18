389e7ffb6bab48dafde9725ba1b894f99472b43a
Aditya Pandey <aditya12bone@gmail.com>
2025-04-18 19:37:31 +0530

    preparing for deployment

---
+++ app/waitlist/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Waitlist = () => {
+  return (
+    <div className="mt-20 fixed w-full">    
+    <div className="aspect-videp w-full">   
+      <iframe
+        style={{width: "100%"}}
+        height="569"
+        src="https://www.create.xyz/app/22353d55-92ca-4fa5-a8c1-6f3342b15420"
+        title="Waiting List Landing Page"
+        frameBorder="0"
+      ></iframe>
+    </div>
+    </div>
+  );
+};
+
+export default Waitlist;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+experimental:
+{
+    serverComponentsHmrCache:false,
+},
+images:
+{
+    remotePatterns:[
+        {
+            protocol:"https",
+            hostname:"vryxupzqnxzzuaixywye.supabase.co"
+        }
+    ]
+},
+    async header()
+    {
+        return[
+            {
+                source:"/embed",
+                headers: [
+                    {key:"Content-Security-Policy",
+                    value:"frame-src 'self  https://aditya.created.app"
+             } ]
+                }
+            
+        ]
+    }
+};
 
 export default nextConfig;
