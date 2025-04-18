4cf856bb9729e93ae4eb9c9e7f4454dafa506f22
Aditya Pandey <aditya12bone@gmail.com>
2025-04-18 20:28:41 +0530

    Updated next.config, fixed Prisma issues, and cleaned build process
    dsdsd
    
    wq

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsHmrCache: false,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "vryxupzqnxzzuaixywye.supabase.co",
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/embed",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "frame-src 'self' https://aditya.created.app",
-          },
-        ],
-      },
-    ];
-  },
-};
-
-export default nextConfig;
+    experimental: {
+      serverComponentsHmrCache: false,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "vryxupzqnxzzuaixywye.supabase.co",
+        },
+      ],
+    },
+    async headers() {
+      return [
+        {
+          source: "/embed",
+          headers: [
+            {
+              key: "Content-Security-Policy",
+              value: "frame-src 'self' https://aditya.created.app",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
