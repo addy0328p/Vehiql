6d631d3cd4c2acf2828ed3297d3e0252a0cc7d8b
Aditya Pandey <aditya12bone@gmail.com>
2025-04-18 20:08:00 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-experimental:
-{
-    serverComponentsHmrCache:false,
-},
-images:
-{
-    remotePatterns:[
-        {
-            protocol:"https",
-            hostname:"vryxupzqnxzzuaixywye.supabase.co"
-        }
-    ]
-},
-    async header()
-    {
-        return[
-            {
-                source:"/embed",
-                headers: [
-                    {key:"Content-Security-Policy",
-                    value:"frame-src 'self  https://aditya.created.app"
-             } ]
-                }
-            
-        ]
-    }
+  experimental: {
+    serverComponentsHmrCache: false,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vryxupzqnxzzuaixywye.supabase.co",
+      },
+    ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://aditya.created.app",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
